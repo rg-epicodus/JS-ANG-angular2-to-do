43ecd07e69c10909ec43e0ea3fdb8be013aea14f
Rich Garrick <richg341@gmail.com>
2017-09-18 18:35:23 -0700

    add dynamic task display

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-      <ul>
-        <li>{{firstTask.description}}</li>
-      </ul>
-    </div>
-  `
+  <div class="container">
+   <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+   <h3>{{currentFocus}}</h3>
+
+   <ul>
+     <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+   </ul>
+
+ </div>  `
 })
 
 export class AppComponent {
@@ -19,7 +20,14 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstTask = {
-    description: "Finish weekend Angular homework for Epicodus course"
-  }
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')
+  ];
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) { }
 }
